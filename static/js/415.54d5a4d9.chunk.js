7226b5236fa0571b02109df6c952169aabf4783f
ihor-kutsenko <96702664+ihor-kutsenko@users.noreply.github.com>
2023-08-05 18:49:22 +0000

    Deploying to gh-pages from @ ihor-kutsenko/test-react-browser-router@73fcd3f307cd7fb7b121c5c38c51e14e0690323c 🚀

---
+++ static/js/415.54d5a4d9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("main",{children:(0,c.jsx)("h1",{children:"Welcome "})})}}}]);
+//# sourceMappingURL=415.54d5a4d9.chunk.js.map
